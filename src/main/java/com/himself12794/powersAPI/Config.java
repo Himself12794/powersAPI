0245deca4e00d823a3cf52d23c8e612b5bf0d342
Himself12794 <pwhiting.bba@gmail.com>
2015-05-27 22:45:24 -0400

    added config

---
+++ src/main/java/com/himself12794/powersAPI/Config.java
@@ -0,0 +1,47 @@
+package com.himself12794.powersAPI;
+
+import net.minecraftforge.common.config.ConfigCategory;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class Config {
+	
+	public static ConfigCategory spellsConfig;
+	public static int flamethrowing;
+	public static int instantSpellRange;
+	
+	public static void loadConfig(FMLPreInitializationEvent event ) {
+		
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		
+		
+		
+		//Property flames = config.get(spells, "FlamethrowingGriefing");
+		
+		config.load();
+		
+		/*Config for Spell Options*/
+		ConfigCategory spells = config.getCategory("Spells");
+		spells.setComment("Configuration for spells");
+		
+		// Flamethrowing Griefing Config Option
+		Property flames = new Property("FlamethrowingGriefing", "1", Property.Type.INTEGER);
+		flames.setMinValue(0);
+		flames.setMaxValue(2);
+		flames.comment = "Flamethrowing griefing: 0=none, 1=vines & grass, 2=all burnable blocks";
+		spells.put("FlamethrowingGriefing", flames);
+		flamethrowing = flames.getInt();
+		
+		// Instant Spell Range Option
+		Property instantSpell = new Property("InstantSpellRange", "50", Property.Type.INTEGER);
+		instantSpell.setMinValue(1);
+		instantSpell.setMaxValue(100);
+		instantSpell.comment = "The range for instant spells. Max is 100 blocks, min is 1";
+		spells.put("InstantSpellRange", instantSpell);
+		instantSpellRange = instantSpell.getInt();
+		
+		config.save();
+	}
+	
+}
