5ad9c26ffe8fe07866391134359d6ede76e7cc3e
Himself12794 <pwhiting.bba@gmail.com>
2015-08-19 23:33:24 -0400

    updated constants

--- src/main/java/com/himself12794/powersapi/util/Reference.java
+++ src/main/java/com/himself12794/powersapi/util/Reference.java
@@ -8,9 +8,9 @@ public class Reference {
     
     public static class TagIdentifiers {
     	
-    	public static final String power = MODID + ".power.currentPower";
-    	public static final String powerCooldowns = MODID + ".power.powerCooldowns";
-    	public static final String powerEffects = MODID + ".power.powerEffects";
+    	public static final String POWER_CURRENT = MODID + ".power.currentPower";
+    	public static final String POWER_COOLDOWNS = MODID + ".power.powerCooldowns";
+    	public static final String POWER_EFFECTS = MODID + ".power.powerEffects";
     	
     }
     
